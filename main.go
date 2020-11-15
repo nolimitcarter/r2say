0c34685a6c63e9527c8d64a6d62a069b59538838
carterthayer@protonmail.com <carterthayer@protonmail.com>
2020-11-15 13:50:03 -0500

    added ability to print text back to user

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	info, _ := os.Stdin.Stat()
+
+	if info.Mode()&os.ModeCharDevice != 0 {
+		fmt.Println("The command is intended to work with pipes.")
+		fmt.Println("Usage: dig | r2say")
+		return
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	var output []rune
+
+	for {
+		input, _, err := reader.ReadRune()
+		if err != nil && err == io.EOF {
+			break
+		}
+		output = append(output, input)
+	}
+
+	for j := 0; j < len(output); j++ {
+		fmt.Printf("%c", output[j])
+	}
+
+}
